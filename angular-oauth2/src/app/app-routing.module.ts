68941108f2583eef271eba29fbd0069e34fcdeac
riyaz <rrasiyani@slb.com>
2021-11-01 21:03:13 -0500

    add token service, auth service, Http Interceptor, routing, auth guard

---
+++ angular-oauth2/src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './auth.guard';
+import { LoginComponent } from './login/login.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { RegisterComponent } from './register/register.component';
+import { SecureComponent } from './secure/secure.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'secure', pathMatch: 'full' },
+  { path: 'secure', canActivate: [ AuthGuard], component: SecureComponent },
+  { path: 'login', component: LoginComponent},
+  { path: 'register', component: RegisterComponent},
+  { path: '404', component: NotFoundComponent},
+  { path: '**', redirectTo: '404'}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
